7264c93f93d75086c84f5987ab7ccfdab80e5be4
gdkrmr <gkraemer@bgc-jena.mpg.de>
2017-01-10 17:47:36 +0100

    added fast and random krr

--- src/krr.jl
+++ src/krr.jl
@@ -9,12 +9,85 @@ type krr{T}
     ϕ :: MLKernels.MercerKernel{T}
 end
 
+
+type fast_krr{T}
+    λ :: T
+    n :: Int
+    X :: Array{StridedMatrix, 1}
+    α :: Array{StridedVector, 1}
+    ϕ :: MLKernels.MercerKernel{T}
+end
+
+type random_krr{T}
+    λ :: T
+    K :: Int
+    W :: StridedMatrix{T}
+    p :: StridedVector{T}
+end
+
+# weighted sum of random kitchen sinks procedure
+function random_krr{T}(X::StridedMatrix{T}, y::StridedVector{T}, λ::T,
+                       K::Int, σ::T)
+    n, d = size(X)
+    W = randn(d, K)/σ
+    Z = exp(X * W * 1im) # Kxd matrix
+    Z2 = Z' * Z
+    for i in 1:K
+        Z2[i, i] += λ
+    end
+    p = real(cholfact!(Z2) \ (Z' * y))
+    random_krr(λ, K, W, p)
+end
+
+function fit{T}(random_krr::random_krr, X::StridedMatrix{T})
+    real(exp(X * random_krr.W * 1im) * random_krr.p)
+end
+
+function fast_krr{T}(X::StridedMatrix{T}, y::StridedVector{T}, λ::T,
+                     nblocks::Int, ϕ::MLKernels.Kernel{T})
+    n, d = size(X)
+    if(nblocks > n^0.33) warn("nblocks > n^1/3, above theoretical limit") end
+    if(nblocks > n^0.45) warn("nblocks > n^0.45, above empirical limit")  end
+    XX = Vector{StridedMatrix}(nblocks)
+    aa = Vector{StridedVector}(nblocks)
+
+    perm_idxs  = shuffle(1:n)
+    blocksizes = make_blocks(n, nblocks)
+
+    b_start = 1
+    b_end   = 0
+    for i in 1:nblocks
+        b_start = b_end + 1
+        b_end  += blocksizes[i]
+        i_idxs = perm_idxs[b_start:b_end]
+        i_krr = krr(X[i_idxs, :], y[i_idxs], λ, ϕ)
+
+        XX[i] = i_krr.X
+        aa[i] = i_krr.α
+    end
+
+    fast_krr(λ, nblocks, XX, aa, ϕ)
+end
+
+function fit{T}(fast_krr::fast_krr{T}, X::StridedMatrix{T})
+    n, d = size(X)
+    pred = zeros(T, n)
+    for i in 1:fast_krr.n
+        pred += fit(krr(fast_krr.λ, fast_krr.X[i], fast_krr.α[i], fast_krr.ϕ),  X)
+    end
+    pred /= fast_krr.n
+    pred
+end
+
+
 function krr{T}(X::StridedMatrix{T}, y::StridedVector{T}, λ::T,
-                ϕ::MLKernels.Kernel)
+                ϕ::MLKernels.Kernel{T})
     n, d = size(X)
     K = MLKernels.kernelmatrix(ϕ, X)
     for i = 1:n
-        K[i, i] += λ
+        # the n is important to make things comparable between fast and normal
+        # krr
+        K[i, i] += n * λ
     end
 
     α = cholfact!(K) \ y
@@ -22,13 +95,21 @@ function krr{T}(X::StridedMatrix{T}, y::StridedVector{T}, λ::T,
     krr(λ, X, α, ϕ)
 end
 
-function fit{T}(model::krr, X::StridedMatrix{T})
-    @show size(X)
-    @show size(model.X)
-    k = MLKernels.kernelmatrix(model.ϕ, X, model.X)
-    @show size(k)
-    @show size(model.α)
-    k * model.α
+function fit{T}(krr::krr{T}, X::StridedMatrix{T})
+    k = MLKernels.kernelmatrix(krr.ϕ, X, krr.X)
+    k * krr.α
+end
+
+range(x) = minimum(x), maximum(x)
+
+function make_blocks(nobs, nblocks)
+    maxbs, rest = divrem(nobs, nblocks)
+
+    res = fill(maxbs, nblocks)
+    if rest > 0
+        res[1:rest] = maxbs + 1
+    end
+    res
 end
 
 end # module KRR
--- test/test.jl
+++ test/test.jl
@@ -6,21 +6,50 @@ using MLKernels
 using Regression
 using Gadfly
 
-x = rand(1000, 1) * 2 * π - π
-y = vec(sinc.(4 .* x) .+ 0.2 .* sin.(30 .* x) .+ 0.1randn(1000))
+x = rand(5000, 1) * 2 * π - π
+yy = vec(sinc.(4 .* x) .+ 0.2 .* sin.(30 .* x))
+y = yy .+ 0.1randn(5000)
 xnew = collect(-4:0.01:4)''
 
 include("src/krr.jl")
-@time mykrr = KRR.krr(x, y, 4., MLKernels.GaussianKernel(100));
-@time ynew  = KRR.fit(mykrr, xnew);
+@time mykrr     = KRR.krr(       x, y, 4/5000,      MLKernels.GaussianKernel(100.0));
+@time ynew     = KRR.fit(mykrr, xnew)
 
-plot(layer(x = xnew, y = ynew,    Geom.line, Theme(default_color = colorant"green")),
-     layer(x = x,    y = mykrr.α, Geom.line, Theme(default_color = colorant"red")),
-     layer(x = x,    y = y,       Geom.line, Theme(default_color = colorant"blue")))
+@time myfastkrr = KRR.fast_krr(  x, y, 4/5000, 10, MLKernels.GaussianKernel(100.0));
+@time yfastnew = KRR.fit(myfastkrr, xnew)
+
+@time myrandkrr = KRR.random_krr(x, y, 4/5000, 5000 , 0.01)
+@time yrandnew = KRR.fit(myrandkrr, xnew)
+
+KRR.range(ynew - yfastnew)
+KRR.range(ynew - yrandnew)
+
+plot(
+    layer(x = xnew, y = yrandnew, Geom.line, Theme(default_color = colorant"red")),
+    layer(x = xnew, y = yfastnew, Geom.line, Theme(default_color = colorant"yellow")),
+    layer(x = xnew, y = ynew,     Geom.line, Theme(default_color = colorant"green")),
+    layer(x = x,    y = yy,       Geom.line, Theme(default_color = colorant"blue")),
+    Coord.cartesian(ymin = -2, ymax = 2)
+)
 
 
+tmp = rand(10, 10)
+y = rand(10)
+tmp1 = ( tmp' + tmp )
+for i in 1:10 tmp1[i,i] += 10 end
+tmp2 = deepcopy(tmp1)
+@time cholfact!(tmp1)
+@time tmp3 = cholfact(tmp2)
+tmp1
+tmp3
+
+cholfact!(tmp1) \ y
+cholfact(tmp2) \ y
 
 
 (MLKernels.GaussianKernel <: MLKernels.MercerKernel)
 MLKernels.MercerKernel <: MLKernels.Kernel
+
+reload("Regression")
+Regression.ridgereg(tmp, y, 0)
 
