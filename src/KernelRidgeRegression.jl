fab9c76abb7eac9826d8e626c872a5a880d350ab
gdkrmr <gkraemer@bgc-jena.mpg.de>
2017-01-19 16:25:35 +0100

    types are working as intended

--- src/KernelRidgeRegression.jl
+++ src/KernelRidgeRegression.jl
@@ -4,179 +4,36 @@ import MLKernels
 import StatsBase
 import StatsBase: fit, fitted, predict
 
-abstract AbstractKRR <: StatsBase.RegressionModel
+abstract AbstractKRR{T} <: StatsBase.RegressionModel
 
-function fitted(KRR::AbstractKRR)
+function StatsBase.fitted(KRR::AbstractKRR)
     predict(KRR, KRR.X)
 end
 
-type KRR{T <: AbstractFloat} <: AbstractKRR
+type KRR{T <: AbstractFloat} <: AbstractKRR{T}
     λ :: T
-    X :: StridedMatrix{T}
-    α :: StridedVector{T}
+    X :: Matrix{T}
+    α :: Vector{T}
     ϕ :: MLKernels.MercerKernel{T}
+    function KRR(λ, X, α, ϕ)
+        @assert λ > zero(λ)
+        @assert size(X, 1) == length(α)
+        new(λ, X, α, ϕ)
+    end
 end
 
-function KRR{T}(
-    λ::T, X::Array{StridedMatrix, 1},
-    α::Array{StridedVector, 1}, ϕ::MLKernels.MercerKernel{T}
-)
-    @assert λ > zero(T)
-    @assert size(X, 1) == length(α)
-    FastKRR(λ, X, α, ϕ)
-end
-
-
-type FastKRR{T <: AbstractFloat} <: AbstractKRR
-    λ :: T
-    n :: Int
-    X :: Array{StridedMatrix, 1}
-    α :: Array{StridedVector, 1}
+function KRR{T <: AbstractFloat}(
+    λ :: T,
+    X :: Matrix{T},
+    α :: Vector{T},
     ϕ :: MLKernels.MercerKernel{T}
-end
-
-function FastKRR{T <: AbstractFloat}(
-    λ::T, n::Int, X::Array{StridedMatrix, 1},
-    α::Array{StridedVector, 1}, ϕ::MLKernels.MercerKernel{T}
 )
-    @assert λ > zero(T)
-    @assert n > zero(Int)
-    @assert length(X) == length(α)
-    for i in 1:length(X)
-        @assert size(X[i], 1) == length(α[i])
-    end
-    FastKRR(λ, n, X, α, ϕ)
+    KRR{T}(λ, X, α, ϕ)
 end
-
-
-type RandomKRR{T <: AbstractFloat} <: AbstractKRR
-    λ :: T
-    K :: Int
-    W :: StridedMatrix{T}
-    α :: StridedVector{T}
-    Φ :: Function
-end
-
-function RandomKRR{T<:AbstractFloat}(
-    λ::T, K::Int, W::StridedMatrix{T},
-    α::StridedVector{T}, Φ::Functon
-)
-    @assert λ > zero(T)
-    @assert K > zero(Int)
-    @assert size(W, 1) == length(α)
-    RandomKRR(λ, K, W, α, Φ)
-end
-
-
-type TruncNewtKRR{T <: AbstractFloat}
-    λ :: T
-    X :: StridedMatrix{T}
-    α :: StridedVector{T}
-    ϕ :: MLKernels.MercerKernel{T}
-    ɛ :: T
-    max_iter :: Int
-end
-
-function TruncNewtKRR{T<:AbstractFloat}(
-    λ::T, X::StridedMatrix{T}, α::StridedVector{T},
-    ϕ::MLKernels.MercerKernel{T}, ɛ::T, max_iter::Int
-)
-    @assert size(X, 1) == length(α)
-    @assert λ > zero(T)
-    @assert ɛ > zero(T)
-    @assert max_iter > zero(Int)
-    TruncNewtKRR(λ, X, α, ϕ, ɛ, max_iter)
-end
-
-function fit{T<:AbstractFloat}(
-    Type{TruncNewtKRR}, X::StridedMatrix{T}, y::StridedVector{T},
-    λ::T, ϕ::MLKernels.Kernel{T}, ɛ::T = 0.5, max_iter::Int = 200
-)
-    n, d = size(X)
-    K = MLKernels.kernelmatrix(ϕ, X)
-    for i = 1:n
-        # the n is important to make things comparable between fast and normal
-        # KRR
-        @inbounds K[i, i] += n * λ
-    end
-
-    α = truncated_newton!(K, y, zeros(y), ɛ, max_iter)
-
-    TruncNewtKRR(λ, X, α, ϕ, ɛ, max_iter)
-end
-
-function predict{T<:AbstractFloat}(KRR::TruncNewtKRR{T}, X::StridedMatrix{T})
-    k = MLKernels.kernelmatrix(KRR.ϕ, X, KRR.X)
-    k * KRR.α
-end
-
-
-
-function fit{T<:AbstractFloat}(
-    Type{RandomKRR{T}}, X::StridedMatrix{T}, y::StridedVector{T},
-    λ::T, K::Int, σ::T, Φ::Function = (X, W) -> exp(X * W * 1im)
-)
-    n, d = size(X)
-    W = randn(d, K)/σ
-    Z = Φ(X, W) # Kxd matrix
-    Z2 = Z' * Z
-    for i in 1:K
-        @inbounds Z2[i, i] += λ / K
-    end
-    α = real(cholfact(Z2) \ (Z' * y))
-    RandomKRR(λ, K, W, α, Φ)
-end
-
-function predict{T <: AbstractFloat}(RandomKRR::RandomKRR, X::StridedMatrix{T})
-    real(RandomKRR.Φ(X, RandomKRR.W) * RandomKRR.α)
-end
-
-
 
 function fit{T <: AbstractFloat}(
-    Type{FastKRR{T}}, X::StridedMatrix{T}, y::StridedVector{T},
-    λ::T, nblocks::Int, ϕ::MLKernels.Kernel{T}
-)
-    n, d = size(X)
-    nblocks > n^0.33 && warn("nblocks > n^1/3, above theoretical limit ($(n^(1/3)))")
-    nblocks > n^0.45 && warn("nblocks > n^0.45, above empirical limit ($(n^0.45))")
-    XX = Vector{StridedMatrix}(nblocks)
-    aa = Vector{StridedVector}(nblocks)
-
-    perm_idxs  = shuffle(1:n)
-    blocksizes = make_blocks(n, nblocks)
-
-    b_start = 1
-    b_end   = 0
-    for i in 1:nblocks
-        b_start = b_end + 1
-        b_end  += blocksizes[i]
-        i_idxs = perm_idxs[b_start:b_end]
-        i_krr = KRR(X[i_idxs, :], y[i_idxs], λ, ϕ)
-
-        XX[i] = i_krr.X
-        aa[i] = i_krr.α
-    end
-
-    FastKRR(λ, nblocks, XX, aa, ϕ)
-end
-
-fitted(obj::FastKRR) = error("fitted is not defined for $(typeof(obj))")
-
-function predict{T<:AbstractFloat}(FastKRR::FastKRR{T}, X::StridedMatrix{T})
-    n, d = size(X)
-    pred = zeros(T, n)
-    for i in 1:FastKRR.n
-        pred += fit(KRR(FastKRR.λ, FastKRR.X[i], FastKRR.α[i], FastKRR.ϕ),  X)
-    end
-    pred /= FastKRR.n
-    pred
-end
-
-
-function fit{T <: AbstractFloat}(
-    Type{KRR{T}}, X::StridedMatrix{T}, y::StridedVector{T}, λ::T,
-    ϕ::MLKernels.Kernel{T}
+    ::Type{KRR}, X::Matrix{T}, y::Vector{T},
+    λ::T, ϕ::MLKernels.Kernel{T}
 )
     n, d = size(X)
     K = MLKernels.kernelmatrix(ϕ, X)
@@ -191,11 +48,176 @@ function fit{T <: AbstractFloat}(
     KRR(λ, X, α, ϕ)
 end
 
-function predict{T <: AbstractFloat}(KRR::KRR{T}, X::StridedMatrix{T})
+function predict{T <: AbstractFloat}(KRR::KRR{T}, X::Matrix{T})
     k = MLKernels.kernelmatrix(KRR.ϕ, X, KRR.X)
     k * KRR.α
 end
 
+
+
+type FastKRR{T <: AbstractFloat} <: AbstractKRR{T}
+    λ :: T
+    n :: Int
+    X :: Vector{Matrix{T}}
+    α :: Vector{Vector{T}}
+    ϕ :: MLKernels.MercerKernel{T}
+
+    function FastKRR(λ, n, X, α, ϕ)
+        @assert λ > zero(λ)
+        @assert n > zero(n)
+        @assert length(X) == length(α)
+        for i in 1:length(X)
+            @assert size(X[i], 1) == length(α[i])
+        end
+        new(λ, n, X, α, ϕ)
+    end
+end
+
+function FastKRR{T <: AbstractFloat}(
+    λ :: T,
+    n :: Int,
+    X :: Vector{Matrix{T}},
+    α :: Vector{Vector{T}},
+    ϕ :: MLKernels.MercerKernel{T}
+)
+    FastKRR{T}(λ, n, X, α, ϕ)
+end
+
+function StatsBase.fit{T <: AbstractFloat}(
+    ::Type{FastKRR}, X::Matrix{T}, y::Vector{T},
+    λ::T, nblocks::Int, ϕ::MLKernels.Kernel{T}
+)
+    n, d = size(X)
+    nblocks > n^0.33 && warn("nblocks > n^1/3, above theoretical limit ($(n^(1/3)))")
+    nblocks > n^0.45 && warn("nblocks > n^0.45, above empirical limit ($(n^0.45))")
+
+    XX = Vector{Matrix{T}}(nblocks)
+    aa = Vector{Vector{T}}(nblocks)
+
+    perm_idxs  = shuffle(1:n)
+    blocksizes = make_blocks(n, nblocks)
+
+    b_end   = 0
+    for i in 1:nblocks
+        b_start = b_end + 1
+        b_end  += blocksizes[i]
+
+        i_idxs = perm_idxs[b_start:b_end]
+        i_krr  = fit(KRR, X[i_idxs, :], y[i_idxs], λ, ϕ)
+
+        XX[i] = i_krr.X
+        aa[i] = i_krr.α
+    end
+    FastKRR(λ, nblocks, XX, aa, ϕ)
+end
+
+fitted(obj::FastKRR) = error("fitted is not defined for $(typeof(obj))")
+
+function predict{T<:AbstractFloat}(FastKRR::FastKRR{T}, X::Matrix{T})
+    n, d = size(X)
+    pred = zeros(T, n)
+    for i in 1:FastKRR.n
+        pred += predict(KRR(FastKRR.λ, FastKRR.X[i], FastKRR.α[i], FastKRR.ϕ),  X)
+    end
+    pred /= FastKRR.n
+    pred
+end
+
+
+type RandomKRR{T <: AbstractFloat} <: AbstractKRR{T}
+    λ :: T
+    K :: Int
+    W :: Matrix{T}
+    α :: Vector{T}
+    Φ :: Function
+
+    function RandomKRR(λ, K, W, α, Φ)
+        @assert λ > zero(T)
+        @assert K > zero(Int)
+        @assert size(W, 2) == K
+        new(λ, K, W, α, Φ)
+    end
+end
+
+function RandomKRR{T}(
+    λ :: T,
+    K :: Int,
+    W :: Matrix{T},
+    α :: Vector{T},
+    Φ :: Function
+)
+    RandomKRR{T}(λ, K, W, α, Φ)
+end
+
+function fit{T<:AbstractFloat}(
+    ::Type{RandomKRR}, X::Matrix{T}, y::Vector{T},
+    λ::T, K::Int, σ::T, Φ::Function = (X, W) -> exp(X * W * 1im)
+)
+    n, d = size(X)
+    W = randn(d, K)/σ
+    Z = Φ(X, W) # Kxd matrix
+    Z2 = Z' * Z
+    for i in 1:K
+        @inbounds Z2[i, i] += λ / K
+    end
+    α = real(cholfact(Z2) \ (Z' * y))
+    RandomKRR(λ, K, W, α, Φ)
+end
+
+function predict{T <: AbstractFloat}(RandomKRR::RandomKRR, X::Matrix{T})
+    real(RandomKRR.Φ(X, RandomKRR.W) * RandomKRR.α)
+end
+
+
+type TruncatedNewtonKRR{T <: AbstractFloat} <: AbstractKRR{T}
+    λ        :: T
+    X        :: Matrix{T}
+    α        :: Vector{T}
+    ϕ        :: MLKernels.MercerKernel{T}
+    ɛ        :: T
+    max_iter :: Int
+
+    function TruncatedNewtonKRR(λ, X, α, ϕ, ɛ, max_iter)
+        @assert size(X, 1) == length(α)
+        @assert λ > zero(T)
+        @assert ɛ > zero(T)
+        @assert max_iter > zero(Int)
+        new(λ, X, α, ϕ, ɛ, max_iter)
+    end
+end
+
+function TruncatedNewtonKRR{T}(
+    λ        :: T,
+    X        :: Matrix{T},
+    α        :: Vector{T},
+    ϕ        :: MLKernels.MercerKernel{T},
+    ɛ        :: T,
+    max_iter :: Int
+)
+    TruncatedNewtonKRR{T}(λ, X, α, ϕ, ɛ, max_iter)
+end
+
+function fit{T <: AbstractFloat}(
+    ::Type{TruncatedNewtonKRR}, X::Matrix{T}, y::Vector{T},
+    λ::T, ϕ::MLKernels.Kernel{T}, ɛ::T = 0.5, max_iter::Int = 200
+)
+    n, d = size(X)
+    K = MLKernels.kernelmatrix(ϕ, X)
+    for i = 1:n
+        # the n is important to make things comparable between fast and normal
+        # KRR
+        @inbounds K[i, i] += n * λ
+    end
+
+    α = truncated_newton!(K, y, zeros(y), ɛ, max_iter)
+
+    TruncatedNewtonKRR(λ, X, α, ϕ, ɛ, max_iter)
+end
+
+function predict{T<:AbstractFloat}(KRR::TruncatedNewtonKRR{T}, X::Matrix{T})
+    k = MLKernels.kernelmatrix(KRR.ϕ, X, KRR.X)
+    k * KRR.α
+end
 
 
 # utility functions 
@@ -214,8 +236,8 @@ end
 # the truncated newton method for matrix inversion
 # adapted from https://en.wikipedia.org/wiki/Conjugate_gradient_method
 # solves Ax = b for x, overwrites x
-function truncated_newton!{T}(A::StridedMatrix{T}, b::StridedVector{T},
-                              x::StridedVector{T}, ɛ::T, max_iter::Int)
+function truncated_newton!{T}(A::Matrix{T}, b::Vector{T},
+                              x::Vector{T}, ɛ::T, max_iter::Int)
     r = b - A*x
     p = deepcopy(r)
     Ap = deepcopy(r)
--- test/test.jl
+++ test/test.jl
@@ -1,8 +1,4 @@
-include("src/krr.jl")
-
-using Distances
 using BenchmarkTools
-using MLKernels
 using Gadfly
 
 x = rand(5000, 1) * 2 * π - π
@@ -10,34 +6,36 @@ yy = vec(sinc.(4 .* x) .+ 0.2 .* sin.(30 .* x))
 y = yy .+ 0.1randn(5000)
 xnew = collect(-4:0.01:4)''
 
-include("src/KRR.jl")
-@time mykrr     = KRR.krr(       x, y, 4/5000,      MLKernels.GaussianKernel(100.0));
-@time ynew     = KRR.fit(mykrr, xnew);
+reload("KernelRidgeRegression")
 
-@time myfastkrr = KRR.fast_krr(  x, y, 4/5000, 10, MLKernels.GaussianKernel(100.0));
-@time yfastnew = KRR.fit(myfastkrr, xnew);
+@time mykrr = KernelRidgeRegression.fit(KernelRidgeRegression.KRR, x, y, 4/5000, MLKernels.GaussianKernel(100.0));
+@time ynew = KernelRidgeRegression.predict(mykrr, xnew);
 
-@time mytnkrr = KRR.tn_krr(x, y, 4/5000, MLKernels.GaussianKernel(100.0); ɛ = 0.5, max_iter = 200);
-@time ytnnew = KRR.fit(mytnkrr, xnew);
+@time myfastkrr = KernelRidgeRegression.fit(KernelRidgeRegression.FastKRR, x, y, 4/5000, 10, MLKernels.GaussianKernel(100.0));
+@time yfastnew = KernelRidgeRegression.predict(myfastkrr, xnew);
 
-@time myrandkrr = KRR.random_krr(x, y, 4/5000, 500 , 0.01);
-@time yrandnew = KRR.fit(myrandkrr, xnew);
+@time mytnkrr = KernelRidgeRegression.fit(KernelRidgeRegression.TruncatedNewtonKRR, x, y, 4/5000, MLKernels.GaussianKernel(100.0), 0.5, 200);
+@time ytnnew = KernelRidgeRegression.predict(mytnkrr, xnew);
+
+@time myrandkrr = KernelRidgeRegression.fit(KernelRidgeRegression.RandomKRR, x, y, 4/5000, 500 , 0.01)
+@time yrandnew = KernelRidgeRegression.predict(myrandkrr, xnew);
 
 # tanh makes the whole thing rotation-symetric and go through the origin
-@time myrandkrr2 = KRR.random_krr(x, y, 4/5000, 2000, 1.0, (X, W) -> tanh(X * W))
-@time yrandnew2 = KRR.fit(myrandkrr2, xnew);
+@time myrandkrr2 = KernelRidgeRegression.fit(KernelRidgeRegression.RandomKRR, x, y, 4/5000, 2000, 1.0, (X, W) -> tanh(X * W))
+@time yrandnew2 = KernelRidgeRegression.predict(myrandkrr2, xnew);
 
-@time myrandkrr3 = KRR.random_krr(x, y, 4/5000, 2000, 1.0, (X, W) -> 1 ./ ((X * W) .^ 2 + 1))
-@time yrandnew3 = KRR.fit(myrandkrr3 , xnew);
+@time myrandkrr3 = KernelRidgeRegression.fit(KernelRidgeRegression.RandomKRR, x, y, 4/5000, 2000, 1.0, (X, W) -> 1 ./ ((X * W) .^ 2 + 1))
+@time yrandnew3 = KernelRidgeRegression.predict(myrandkrr3 , xnew);
 
-KRR.range(ynew - yfastnew)
-KRR.range(ynew - yrandnew)
-KRR.range(ynew - ytnnew)
+
+KernelRidgeRegression.range(ynew - yfastnew)
+KernelRidgeRegression.range(ynew - yrandnew)
+KernelRidgeRegression.range(ynew - ytnnew)
 
 plot(
     layer(x = xnew, y = ytnnew,    Geom.line, Theme(default_color = colorant"purple")),
-    # layer(x = xnew, y = yrandnew,  Geom.line, Theme(default_color = colorant"red")),
-    # layer(x = xnew, y = yfastnew,  Geom.line, Theme(default_color = colorant"yellow")),
+    layer(x = xnew, y = yrandnew,  Geom.line, Theme(default_color = colorant"red")),
+    layer(x = xnew, y = yfastnew,  Geom.line, Theme(default_color = colorant"yellow")),
     layer(x = xnew, y = ynew,      Geom.line, Theme(default_color = colorant"green")),
     # layer(x = x,    y = yy,        Geom.line, Theme(default_color = colorant"blue")),
     Coord.cartesian(ymin = -1.5, ymax = 1.5)
@@ -55,8 +53,8 @@ tmp2 = deepcopy(tmp1)
 
 @time res1 = cholfact(tmp2) \ y
 @time res2 = tmp2 \ y;
-@time res3 = KRR.truncated_newton!(tmp2, y, randn(size(y)), 0.5, 200);
-@time res4 = KRR.truncated_newton2!(tmp2, y, randn(size(y)), 0.5, 200);
+@time res3 = KernelRidgeRegression.truncated_newton!(tmp2, y, randn(size(y)), 0.5, 200);
+@time res4 = KernelRidgeRegression.truncated_newton2!(tmp2, y, randn(size(y)), 0.5, 200);
 
 res1 - res2
 res2 - res3
@@ -71,4 +69,15 @@ MLKernels.MercerKernel <: MLKernels.Kernel
 reload("Regression")
 Regression.ridgereg(tmp, y, 0)
 
-@code_warntype KRR.krr(x, y, 4/5000, MLKernels.GaussianKernel(100.0))
+@code_warntype KernelRidgeRegression.krr(x, y, 4/5000, MLKernels.GaussianKernel(100.0))
+
+module the_foo
+type foo{T <: AbstractFloat}
+    bar::T
+    function foo(bar)
+        @assert bar > T(2)
+        new(bar)
+    end
+end
+end
+
