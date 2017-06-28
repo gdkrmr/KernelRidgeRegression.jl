853d0e3df28c0423c486656ebf2851b20fe6e6dc
gdkrmr <gkraemer@bgc-jena.mpg.de>
2017-06-28 18:15:53 +0200

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+
 using KernelRidgeRegression
 using MLKernels
 using Base.Test
@@ -5,6 +6,7 @@ using StatsBase
 
 @test GaussianKernel(3.0) == GaussianKernel(3.0)
 
+# Float64
 N = 5000
 x = rand(1, N) * 4π - 2π
 yy = sinc.(x) # vec(sinc.(4 .* x) .+ 0.2 .* sin.(30 .* x))
@@ -25,13 +27,18 @@ myfastkrr = fit(FastKRR,
                 GaussianKernel(100.0))
 yfastnew = predict(myfastkrr, xnew)
 
+myfastkrr2 = fitPar(FastKRR, length(x),
+                    (i) -> x[:, i], (i) -> y[i],
+                    4/5000, 11, GaussianKernel(100.0))
+yfastnew2 = predict(myfastkrr, xnew)
+
 mytnkrr = fit(TruncatedNewtonKRR,
               x, y, 4/5000, GaussianKernel(100.0),
               0.5, 200)
 ytnnew = predict(mytnkrr, xnew)
 
 myrandkrr = fit(RandomFourierFeatures,
-                x, y, 1e-3/5000, 200, 1.0)
+                x, y, 1/1000.0, 300, 1.0)
 yrandnew = predict(myrandkrr, xnew)
 
 emean = sqrt(mean((vec(sinc.(xnew)) - mean(xnew))))
@@ -41,8 +48,26 @@ efast = sqrt(mean((vec(sinc.(xnew)) - yfastnew) .^ 2))
 erand = sqrt(mean((vec(sinc.(xnew)) - yrandnew) .^ 2))
 etn   = sqrt(mean((vec(sinc.(xnew)) - ytnnew) .^ 2))
 
+@test eltype(emean) == Float64
+@test eltype(ekrr ) == Float64
+@test eltype(enyst) == Float64
+@test eltype(efast) == Float64
+@test eltype(erand) == Float64
+@test eltype(etn  ) == Float64
+
 @test emean > ekrr
 @test emean > enyst
 @test emean > efast
 @test emean > erand
 @test emean > etn
+
+# from julia/test/show.jl:
+replstr(x) = sprint((io, y′) -> show(IOContext(io, :limit => true), MIME("text/plain"), y′), x)
+
+@test replstr(mykrr) == "KernelRidgeRegression.KRR{Float64}:\n    λ = 2.0e-7\n    ϕ = SquaredExponentialKernel(100.0)"
+@test contains(replstr(myrandkrr), "KernelRidgeRegression.RandomFourierFeatures{Float64,Complex{Float64}}:\n    λ = 0.001\n:    σ = 1.0\n:    K = 300\n    ϕ = KernelRidgeRegression")
+@test replstr(mynystkrr) == "KernelRidgeRegression.NystromKRR{Float64}:\n    λ = 2.0e-7\n    ϕ = SquaredExponentialKernel(100.0)\n    m = 280"
+@test replstr(myfastkrr) == "KernelRidgeRegression.FastKRR{Float64}:\n    λ = 0.0008\n    m = 11\n    ϕ = SquaredExponentialKernel(100.0)"
+@test replstr(myfastkrr2) == "KernelRidgeRegression.FastKRR{Float64}:\n    λ = 0.0008\n    m = 11\n    ϕ = SquaredExponentialKernel(100.0)"
+@test replstr(mytnkrr) == "KernelRidgeRegression.TruncatedNewtonKRR{Float64}:\n    λ = 0.0008\n    ϕ = SquaredExponentialKernel(100.0)"
+
